ae2817442efa9f94b0e6024127488a1a2bc68677
Velocidensity <118209517+Velocidensity@users.noreply.github.com>
2023-08-11 23:02:36 +0200

    Add loading and refreshing Trakt tokens
    Fix Trakt watched show data path

--- trakt_to_serializd/trakt.py
+++ trakt_to_serializd/trakt.py
@@ -3,7 +3,7 @@ import time
 
 import httpx
 
-from trakt_to_serializd.consts import CLIENT_ID, CLIENT_SECRET
+from trakt_to_serializd.consts import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 from trakt_to_serializd.exceptions import TraktError
 
 BASE_URL = 'https://api.trakt.tv'
@@ -19,9 +19,20 @@ class TraktAPI:
             'trakt-api-version': '2'
         })
 
-    def login(self):
+    def load_token(self, access_token: str):
+        """
+        Loads saved Trakt access token
+
+        Args:
+            access_token: Trakt access token
+        """
+        self.session.headers['Authorization'] = f'Bearer {access_token}'
+
+    def login(self) -> dict:
         """
         Performs OAuth login using a device code
+
+        This is an interactive process, requiring user input
 
         Raises:
             TraktError: Unexpected response
@@ -65,6 +76,55 @@ class TraktAPI:
             raise TraktError
 
         self.session.headers['Authorization'] = f'Bearer {auth_data["access_token"]}'
+        return auth_data
+
+    def refresh_token(self, refresh_token: str) -> dict:
+        """
+        Refreshes user authorization using a refresh token
+
+        Args:
+            refresh_token: Trakt refresh token
+
+        Raises:
+            TraktError: Unexpected response
+        """
+        resp = self.session.post(
+            '/oauth/token',
+            json={
+                'refresh_token': refresh_token,
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+                'redirect_uri': REDIRECT_URI,
+                'grant_type': 'refresh_token'
+            }
+        )
+        if not resp.is_success:
+            self.logger.error(f'Trakt returned status code: {resp.status_code}')
+            self.logger.debug(resp.text)
+            raise TraktError(f'Trakt returned status code: {resp.status_code}')
+
+        auth_data = resp.json()
+        self.session.headers['Authorization'] = f'Bearer {auth_data["access_token"]}'
+        return auth_data
+
+    def get_user_info(self) -> dict:
+        """
+        Fetches user information for the currently logged in user
+
+        Requires authorization.
+
+        Returns:
+            dict: User in formation
+
+        Raises:
+            TraktError: Unexpected response
+        """
+        resp = self.session.get('/users/settings')
+        if not resp.is_success:
+            self.logger.error(f'Trakt returned status code: {resp.status_code}')
+            raise TraktError(f'Trakt returned status code: {resp.status_code}')
+
+        return resp.json()
 
     def get_watched_shows(self, username: str) -> dict:
         """
@@ -79,7 +139,7 @@ class TraktAPI:
         Raises:
             TraktError: Unexpected response
         """
-        resp = self.session.get(f'/users/{username}/watched/movies')
+        resp = self.session.get(f'/users/{username}/watched/shows')
         if not resp.is_success:
             self.logger.error(f'Trakt returned status code: {resp.status_code}')
             raise TraktError(f'Trakt returned status code: {resp.status_code}')
